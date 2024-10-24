b2983df3d125383252e85e28a45afeef6e7f93e7
nymr-jp <naoya.uemura@proton.me>
2024-10-24 23:07:08 +0900

    test push

--- lib/terminal-manipulator/src/parser.rs
+++ lib/terminal-manipulator/src/parser.rs
@@ -5,35 +5,30 @@ pub enum KeyCode {
     Right,
     Up,
     Down,
+    Home,
+    F(u8),
     Something,
 }
 
 pub fn parse(buffer: &[u8]) -> std::io::Result<Option<KeyCode>> {
+    if buffer.is_empty() {
+        return Ok(None);
+    }
+
     match buffer[0] {
         b'\x1B' => {
-            if buffer.len() == 0 {
+            if buffer.len() == 1 {
                 Ok(Some(KeyCode::Esc))
             } else {
                 match buffer[1] {
-                    b'O' => {
-                        if buffer.len() == 2 {
-                            Ok(None)
-                        } else {
-                            match buffer[2] {
-                                b'D' => Ok(Some(KeyCode::Left)),
-                                b'C' => Ok(Some(KeyCode::Right)),
-                                b'A' => Ok(Some(KeyCode::Up)),
-                                b'B' => Ok(Some(KeyCode::Down)),
-                                _ => Ok(None)
-                            }
-                        }
-                    },
                     b'[' => {
+                        // ANSI escape code: xterm sequence
                         match buffer[2] {
                             b'D' => Ok(Some(KeyCode::Left)),
                             b'C' => Ok(Some(KeyCode::Right)),
                             b'A' => Ok(Some(KeyCode::Up)),
                             b'B' => Ok(Some(KeyCode::Down)),
+                            b'H' => Ok(Some(KeyCode::Home)),
                             _ => Ok(None)
                         }
                     },
